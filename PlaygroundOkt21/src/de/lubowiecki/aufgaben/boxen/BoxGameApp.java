6972c3d03a40ac9a867f0f7b38e1d324bc782699
tlubowiecki <tlubowiecki@localhost>
2021-11-03 13:34:26 +0100

    AUfgabe

---
+++ PlaygroundOkt21/src/de/lubowiecki/aufgaben/boxen/BoxGameApp.java
@@ -0,0 +1,12 @@
+package de.lubowiecki.aufgaben.boxen;
+
+public class BoxGameApp {
+	
+	// Schreibe eine Klasse Boxer mit der Methode angriff, verteidigen und einer Eigenschaft kondition;
+	// Bei angrif wird per zufall ein wert ermittelt, verteidigung kann diesen Wert senken, 
+	// Rest wird von der konditoon abgezogen
+	// erreicht konditon 0, dann geht der Boxer KO.
+	// Jeder Boxer läuft im eigen Thread und greift den anderen an.
+	
+
+}
